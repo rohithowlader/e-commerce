23f83042c8a45e66deafc19765eda58f8a16ba30
Rohit Howlader <rohithowlader2017@gmail.com>
2023-09-30 19:04:06 +0530

    DataBase Connection

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 const app = require("./app");
 const dotenv = require("dotenv");
-
+const connectDB = require("./config/database");
 dotenv.config({ path: "backend/config/config.env" });
-
+//Database Connection
+connectDB();
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on http://localhost:${process.env.PORT}`);
 });
